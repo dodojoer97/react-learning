551b430d102ba626bb81c8c5bead18005de424ab
Ido <ido.sarue@travelfactory.fr>
2024-05-02 15:32:14 +0300

    split section 4 proj into components

---
+++ section4/01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,20 @@
+// Components
+import CoreConcept from "./CoreConcept.jsx"
+
+// Data
+import { CORE_CONCEPTS } from "../data.js"
+
+function CoreConcepts() {
+	return (
+		<section id="core-concepts">
+			<h2>Core Concepts</h2>
+			<ul>
+				{CORE_CONCEPTS.map((conceptItem) => (
+					<CoreConcept key={conceptItem.title} {...conceptItem} />
+				))}
+			</ul>
+		</section>
+	)
+}
+
+export default CoreConcepts

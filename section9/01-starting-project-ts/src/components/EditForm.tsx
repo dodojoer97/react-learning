be89309c7de16c8f450e1b0185d2877077ad972b
ido sarue <dodojoker67@gmail.com>
2024-05-11 11:54:39 +0300

    add editing

--- section9/01-starting-project-ts/src/components/EditForm.tsx
+++ section9/01-starting-project-ts/src/components/EditForm.tsx
@@ -7,7 +7,12 @@ import { FormProps } from "@/types/FormProps";
 import Form from "@/components/Form";
 
 const EditForm: React.FC<FormProps> = ({ onCancel, onSave, initialProject, ...props }) => {
-	return <Form {...props} initialProject={initialProject} onSave={onSave} onCancel={onCancel} />;
+	return (
+		<>
+			<Form {...props} initialProject={initialProject} onSave={onSave} onCancel={onCancel} />
+			{/* TO ADD MORE COMPONENTS FOR EDIT */}
+		</>
+	);
 };
 
 export default EditForm;

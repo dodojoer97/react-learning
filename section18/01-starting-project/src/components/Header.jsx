b7c660da8ef8fdf1c4757a8cb71b33aeedbc98ed
ido sarue <dodojoker67@gmail.com>
2024-07-06 17:17:27 +0300

    add things

---
+++ section18/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,19 @@
+// Assets
+import logoImg from "../assets/logo.jpg";
+
+// Components
+import Button from "./UI/Button";
+
+export default function Header() {
+	return (
+		<header id="main-header">
+			<div id="title">
+				<img src={logoImg} alt="restaurant" />
+				<h1>ReactFood</h1>
+			</div>
+			<nav>
+				<Button textOnly>Cart (0)</Button>
+			</nav>
+		</header>
+	);
+}
---
+++ section18/01-starting-project/src/components/MealItem.jsx
@@ -0,0 +1,23 @@
+// Util
+import { currencyFormater } from "../util/formatting";
+
+// Components
+import Button from "./UI/Button";
+
+export default function MealItem({ meal }) {
+	return (
+		<li className="meal-item">
+			<article>
+				<img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
+				<div>
+					<h3>{meal.name}</h3>
+					<p className="meal-item-price">{currencyFormater.format(meal.price)}</p>
+					<p className="meal-item-description">{meal.description}</p>
+				</div>
+				<p className="meal-item-actions">
+					<Button> Add to cart</Button>
+				</p>
+			</article>
+		</li>
+	);
+}

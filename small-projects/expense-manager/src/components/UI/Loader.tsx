db14f2db1e881f7e65e9338d70764409a3b31040
ido sarue <dodojoker67@gmail.com>
2024-07-13 17:29:57 +0300

    start adding loader

--- small-projects/expense-manager/src/components/UI/Loader.tsx
+++ small-projects/expense-manager/src/components/UI/Loader.tsx
@@ -2,7 +2,7 @@
 import { FC } from "react";
 
 const Loader: FC = () => {
-	return <div className="absolute top-0 right-0 bottom-0 left-0 bg-red-400"></div>;
+	return <div className="absolute top-0 right-0 bottom-0 left-0 bg-slate-300 z-10"></div>;
 };
 
 export default Loader;

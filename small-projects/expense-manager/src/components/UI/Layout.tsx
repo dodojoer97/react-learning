b23f86fdc3b0b877840f9445ab51532932c008a7
ido sarue <dodojoker67@gmail.com>
2024-07-13 17:28:28 +0300

    start adding loader

--- small-projects/expense-manager/src/components/UI/Layout.tsx
+++ small-projects/expense-manager/src/components/UI/Layout.tsx
@@ -1,14 +1,23 @@
 // React
-import type { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useContext } from "react";
 
 // Components
 import Header from "@/components/UI/Header";
+import Loader from "@/components/UI/Loader";
+
+// Context
+import { AuthContext } from "@/store/AuthContext";
 
 const Layout: FC<PropsWithChildren> = ({ children }) => {
+	const authCTX = useContext(AuthContext);
 	return (
 		<>
-			<Header />
-			<section className="bg-slate-50 h-svh">{children}</section>
+			<div className="relative">
+				<Header />
+				<section className="bg-slate-50 h-svh">{children}</section>
+
+				{authCTX.loading && <Loader />}
+			</div>
 		</>
 	);
 };
---
+++ small-projects/expense-manager/src/components/UI/Loader.tsx
@@ -0,0 +1,8 @@
+// React
+import { FC } from "react";
+
+const Loader: FC = () => {
+	return <div className="absolute top-0 right-0 bottom-0 left-0 bg-red-400"></div>;
+};
+
+export default Loader;

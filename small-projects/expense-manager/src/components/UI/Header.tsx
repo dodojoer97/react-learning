2fa91ed7b33bee909d1478bdb00c328a430ee4e2
Ido <ido.sarue@travelfactory.fr>
2024-07-16 16:54:56 +0300

    add active classes to navs

--- small-projects/expense-manager/src/components/UI/Header.tsx
+++ small-projects/expense-manager/src/components/UI/Header.tsx
@@ -16,7 +16,8 @@ const Header: FC = () => {
 	const { t } = useTranslation("header")
 
 	// CSS classes
-	const baseNavClasses = "text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
+	const baseNavClasses =
+		"text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800"
 	const activeNavClasses = "bg-gray-200 dark:bg-gray-900"
 
 	const [mobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false)
@@ -45,18 +46,14 @@ const Header: FC = () => {
 						<NavLink
 							to='/login'
 							className={({ isActive }) =>
-								`${baseNavClasses} ${
-									isActive ? activeNavClasses : ""
-								}`
+								`${baseNavClasses} ${isActive ? activeNavClasses : ""}`
 							}>
 							{t("signin")}
 						</NavLink>
 						<NavLink
 							to='/signup'
 							className={({ isActive }) =>
-							`${baseNavClasses} ${
-								isActive ? activeNavClasses : ""
-							}`
+								`${baseNavClasses} ${isActive ? activeNavClasses : ""}`
 							}>
 							{t("signup")}
 						</NavLink>

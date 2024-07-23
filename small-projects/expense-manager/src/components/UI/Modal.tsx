6969e19986dda3f25b55823c7fc4913333dbda0f
ido sarue <dodojoker67@gmail.com>
2024-07-23 20:00:25 +0300

    update modal styling

--- small-projects/expense-manager/src/components/UI/Modal.tsx
+++ small-projects/expense-manager/src/components/UI/Modal.tsx
@@ -1,60 +1,58 @@
 // React
-import type { FC, PropsWithChildren, MouseEvent } from "react"
-import { useRef, useEffect } from "react"
-import { createPortal } from "react-dom"
+import type { FC, PropsWithChildren, MouseEvent } from "react";
+import { useRef, useEffect } from "react";
+import { createPortal } from "react-dom";
 
 // UI components
-import CloseButton from "./CloseButton"
+import CloseButton from "./CloseButton";
 
 // CSS
-import "./Modal.css"
+import "./Modal.css";
 
 interface IModalProps extends PropsWithChildren {
-  isOpen: boolean
-  onClose(): void
+	isOpen: boolean;
+	onClose(): void;
 }
 
 const Modal: FC<IModalProps> = ({ isOpen, children, onClose }) => {
-  const modalRef = useRef<HTMLDivElement | null>(null)
+	const modalRef = useRef<HTMLDivElement | null>(null);
 
-  useEffect(() => {
-    const handleEscapeKey = (event: KeyboardEvent) => {
-      if (event.key === 'Escape') {
-        onClose();
-      }
-    };
+	useEffect(() => {
+		const handleEscapeKey = (event: KeyboardEvent) => {
+			if (event.key === "Escape") {
+				onClose();
+			}
+		};
 
-    document.addEventListener('keydown', handleEscapeKey);
-    return () => {
-      document.removeEventListener('keydown', handleEscapeKey);
-    };
-  }, [onClose]);
+		document.addEventListener("keydown", handleEscapeKey);
+		return () => {
+			document.removeEventListener("keydown", handleEscapeKey);
+		};
+	}, [onClose]);
 
-  const handleBackdropClick = (event: MouseEvent<HTMLDivElement>) => {
-    if (event.target === modalRef.current) {
-      onClose();
-    }
-  };
+	const handleBackdropClick = (event: MouseEvent<HTMLDivElement>) => {
+		if (event.target === modalRef.current) {
+			onClose();
+		}
+	};
 
-  console.log("isOpen: ", isOpen)
+	console.log("isOpen: ", isOpen);
 
-  return createPortal(
-    
-      <div
-        ref={modalRef}
-        className="modal-backdrop flex flex-col justify-center ali"
-        onClick={handleBackdropClick}
-      >
-        <div className="modal bg-slate-100 max-w-[80vw] min-h-[50dvh] rounded-lg shadow-lg">
-          <div className="flex justify-end px-2 py-1">
-            <CloseButton onClose={onClose} />
-          </div>
-          {children}
-        </div>
-      </div>
-   ,
-    document.querySelector("#modal") as HTMLDivElement
-  )
-}
+	return createPortal(
+		<div
+			ref={modalRef}
+			className="modal-backdrop flex flex-col justify-center items-center"
+			onClick={handleBackdropClick}
+		>
+			<div className="modal bg-slate-100 w-[90vw] min-h-[50dvh] rounded-lg shadow-lg">
+				<div className="flex justify-end px-2 py-1">
+					<CloseButton onClose={onClose} />
+				</div>
+				{children}
+			</div>
+		</div>,
+		document.querySelector("#modal") as HTMLDivElement
+	);
+};
 
-export default Modal
+export default Modal;

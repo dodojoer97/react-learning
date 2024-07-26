a75803914cc3621c7ac3d0af61771b5eddd38ceb
ido sarue <dodojoker67@gmail.com>
2024-07-26 13:05:12 +0300

    fix auth not working

--- small-projects/expense-manager/backend/src/config/firebase.ts
+++ small-projects/expense-manager/backend/src/config/firebase.ts
@@ -1,10 +1,11 @@
 import * as admin from "firebase-admin";
 import * as serviceAccount from "../../serviceAccountKey.json";
 
-console.log("serviceAccount: ", serviceAccount);
 admin.initializeApp({
 	credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
 });
+
+process.env.FIREBASE_AUTH_EMULATOR_HOST = "localhost:9099";
 
 const db = admin.firestore();
 db.settings({

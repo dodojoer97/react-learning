885cf4efc8208efd29b5bc2f9562f47b8d5d8f62
ido sarue <dodojoker67@gmail.com>
2024-07-29 08:21:57 +0300

    start working on add category form

---
+++ small-projects/expense-manager/frontend/src/components/AddCategoryForm.tsx
@@ -0,0 +1,21 @@
+// React
+import type { FC } from "react";
+import { useState, useContext } from "react";
+
+// Store
+import { SettingsContext } from "@/store/SettingsContext";
+
+// Copmonents
+import Form from "@/components/UI/Form";
+import Input from "@/components/UI/Input";
+import Button from "@/components/UI/Button";
+import InputError from "@/components/UI/InputError";
+
+// Hooks
+import useInput from "@/hooks/useInput";
+
+const AddCategoryForm: FC = () => {
+	const settingsCTX = useContext(SettingsContext);
+
+	return <Form className="mx-auto mb-0 mt-8 max-w-md space-y-4"></Form>;
+};
--- small-projects/expense-manager/frontend/src/pages/Categories.tsx
+++ small-projects/expense-manager/frontend/src/pages/Categories.tsx
@@ -8,10 +8,6 @@ import { useTranslation } from "react-i18next";
 
 // UI Components
 import Layout from "@/components/UI/Layout";
-import Form from "@/components/UI/Form";
-import Input from "@/components/UI/Input";
-import Button from "@/components/UI/Button";
-import InputError from "@/components/UI/InputError";
 
 // Components
 import CategoryComp from "@/components/Category";
@@ -36,7 +32,7 @@ const Categories: FC = () => {
 			<div className="mx-auto max-w-lg text-center">
 				<h1 className="text-2xl font-bold sm:text-3xl">{t("categoriesTitle")}</h1>
 			</div>
-			<Form className="mx-auto mb-0 mt-8 max-w-md space-y-4"></Form>
+			{/* ADD CATEGORY FORM */}
 
 			<ul>
 				{settingsCTX.categories.map((category: Category) => (

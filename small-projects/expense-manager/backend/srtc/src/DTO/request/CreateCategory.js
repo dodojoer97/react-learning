bc2fed3edd88b79267483f502f18be35f6c5759a
ido sarue <dodojoker67@gmail.com>
2024-07-30 18:01:01 +0300

    add common to frontend and backend

--- small-projects/expense-manager/backend/srtc/src/DTO/request/CreateCategory.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateCategoryDTO {
-    constructor(category, userId) {
-        this.category = category;
-        this.userId = userId;
-    }
-}
-exports.default = CreateCategoryDTO;

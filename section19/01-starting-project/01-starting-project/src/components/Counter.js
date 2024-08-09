18744d15acaa11dd167062d0a3e2a31d049df0ee
ido sarue <dodojoker67@gmail.com>
2024-08-09 08:31:41 +0300

    start

--- section19/01-starting-project/01-starting-project/src/components/Counter.js
+++ section19/01-starting-project/01-starting-project/src/components/Counter.js
@@ -1,19 +1,25 @@
-import classes from './Counter.module.css';
+import classes from "./Counter.module.css";
 
-import store from '../store';
+// Redux
+import { useSelector, connect } from "react-redux";
+
+import store from "../store";
 
 const Counter = () => {
-  const toggleCounterHandler = () => {
-    store.dispatch({type: "increment"})
-  };
+	// Sets up a subscription to the store
+	const counter = useSelector((state) => state.counter);
 
-  return (
-    <main className={classes.counter}>
-      <h1>Redux Counter</h1>
-      <div className={classes.value}>{store.getState().counter}</div>
-      <button onClick={toggleCounterHandler}>Toggle Counter</button>
-    </main>
-  );
+	const toggleCounterHandler = () => {
+		store.dispatch({ type: "increment" });
+	};
+
+	return (
+		<main className={classes.counter}>
+			<h1>Redux Counter</h1>
+			<div className={classes.value}>{counter}</div>
+			<button onClick={toggleCounterHandler}>Toggle Counter</button>
+		</main>
+	);
 };
 
 export default Counter;

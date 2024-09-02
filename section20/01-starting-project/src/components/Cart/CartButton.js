90db12e7c2380269c9cbaaa9561ca475605dec97
Ido <ido.sarue@travelfactory.fr>
2024-09-02 17:41:50 +0300

    start adding

--- section20/01-starting-project/src/components/Cart/CartButton.js
+++ section20/01-starting-project/src/components/Cart/CartButton.js
@@ -1,12 +1,29 @@
 import classes from './CartButton.module.css';
 
+import { useDispatch, useSelector } from 'react-redux';
+import { toggleCart } from '../../store/index'; // Adjust the path as needed
+
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+
+  const dispatch = useDispatch(); // Make sure this line is included
+
+    // Access the number of items in the cart from the Redux state
+    const cartItemCount = useSelector((state) => {
+      return state.items.length
+    });
+  
+    // Function to handle the button click and toggle the cart
+    const toggleCartHandler = () => {
+      dispatch(toggleCart());
+    };
+  
+  
+    return (
+      <button className={classes.button} onClick={toggleCartHandler}>
+        <span>My Cart</span>
+        <span className={classes.badge}>{cartItemCount}</span>
+      </button>
+    );
 };
 
 export default CartButton;

81de6963f29f745862930df6cca6d9c679a4073e
ido sarue <dodojoker67@gmail.com>
2024-09-14 12:28:41 +0300

    move calling of transactions to list

--- small-projects/expense-manager/frontend/src/components/Dashboard/RightActions.tsx
+++ small-projects/expense-manager/frontend/src/components/Dashboard/RightActions.tsx
@@ -6,9 +6,13 @@ import { useContext } from "react";
 import { OpenContext } from "@/store/OpenContext";
 import { TransactionContext, defaultTransaction } from "@/store/TransactionContext";
 
+// Components
+import TransactionPanel from "@/components/Transaction/TransactionPanel";
+import SlidingPanel from "@/components/UI/SlidingPanel";
+
 const RightActions: FC = () => {
 	// Store
-	const { open } = useContext(OpenContext);
+	const { open, isOpen, toggleOpen, close } = useContext(OpenContext);
 	const { selectTransaction } = useContext(TransactionContext);
 	// TODO maybe add to config
 	const dashboardPanelId = "dashboard-panel";
@@ -19,29 +23,38 @@ const RightActions: FC = () => {
 	};
 
 	return (
-		<div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-			{/* Filter button */}
-			{/* <FilterButton align="right" /> */}
-			{/* Datepicker built with flatpickr */}
-			{/* <Datepicker align="right" /> */}
-			<button
-				className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white"
-				onClick={(e) => {
-					e.stopPropagation();
-					handleOpen();
-				}}
-			>
-				<svg
-					className="fill-current shrink-0 xs:hidden"
-					width="16"
-					height="16"
-					viewBox="0 0 16 16"
+		<>
+			<div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
+				{/* Filter button */}
+				{/* <FilterButton align="right" /> */}
+				{/* Datepicker built with flatpickr */}
+				{/* <Datepicker align="right" /> */}
+				<button
+					className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white"
+					onClick={(e) => {
+						e.stopPropagation();
+						handleOpen();
+					}}
 				>
-					<path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-				</svg>
-				<span className="max-xs:sr-only">Add Transaction</span>
-			</button>
-		</div>
+					<svg
+						className="fill-current shrink-0 xs:hidden"
+						width="16"
+						height="16"
+						viewBox="0 0 16 16"
+					>
+						<path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
+					</svg>
+					<span className="max-xs:sr-only">Add Transaction</span>
+				</button>
+			</div>
+			<SlidingPanel
+				isOpen={isOpen(dashboardPanelId)}
+				onClose={() => toggleOpen(dashboardPanelId)}
+				slideDirection="from-right"
+			>
+				<TransactionPanel onSave={() => close(dashboardPanelId)} />
+			</SlidingPanel>
+		</>
 	);
 };
 
--- small-projects/expense-manager/frontend/src/pages/Dashboard.tsx
+++ small-projects/expense-manager/frontend/src/pages/Dashboard.tsx
@@ -7,33 +7,15 @@ import { Transaction } from "@common";
 
 // Components
 import Layout from "@/components/UI/Layout";
-import Button from "@/components/UI/Button";
 import TransactionList from "@/components/Transaction/TransactionList";
-import PeriodSelector from "@/components/Transaction/PeriodSelector";
 import RightActions from "@/components/Dashboard/RightActions";
-import ModalBasic from "@/templates/mosaic/components/ModalBasic";
-import TransactionPanel from "@/components/Transaction/TransactionPanel";
-import SlidingPanel from "@/components/UI/SlidingPanel";
-
-// FontAwesome
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
-
-//
+import TransactionDoughnut from "@/components/Transaction/TransactionDoughnut";
 
 // Store
-import { TransactionContext, defaultTransaction } from "@/store/TransactionContext";
-import { OpenContext } from "@/store/OpenContext";
-import TransactionDoughnut from "@/components/Transaction/TransactionDoughnut";
 
 // TODO add translations
 const Dashboard: FC = () => {
 	console.log("re render Dashboard");
-	// Store
-	const { isOpen, close, toggleOpen } = useContext(OpenContext);
-
-	// TODO maybe add to config
-	const dashboardPanelId = "dashboard-panel";
 
 	return (
 		<>
@@ -42,13 +24,6 @@ const Dashboard: FC = () => {
 					<TransactionDoughnut />
 					<TransactionList />
 				</div>
-				<SlidingPanel
-					isOpen={isOpen(dashboardPanelId)}
-					onClose={() => toggleOpen(dashboardPanelId)}
-					slideDirection="from-right"
-				>
-					<TransactionPanel onSave={() => close(dashboardPanelId)} />
-				</SlidingPanel>
 			</Layout>
 		</>
 	);

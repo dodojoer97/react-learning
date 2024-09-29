8c83b84938b3655197dd81c0f015a9f01734b05e
Ido <ido.sarue@travelfactory.fr>
2024-09-29 16:28:25 +0300

    fisinsh basics

--- section21/01-starting-project/src/pages/Home.js
+++ section21/01-starting-project/src/pages/Home.js
@@ -1,11 +1,20 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 
-function homePage() {
+function HomePage() {
+    const navigate = useNavigate()
+    
+    function navigateHandler() {
+        navigate("/products")
+    }
+
     return <>
         <h1>Home page</h1>
         <p>Got to  <Link to="/products">products</Link></p>
+        <p>
+            <button onClick={navigateHandler}>test</button>
+        </p>
     </>
 
 }
 
-export default homePage
+export default HomePage

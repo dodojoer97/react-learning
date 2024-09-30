e60961dea9dc2d8b198baa8a3ea73abfd07ff52e
Ido <ido.sarue@travelfactory.fr>
2024-09-30 17:26:13 +0300

    add error handling

--- section21/12-adv-starting-project/frontend/src/pages/Error.jsx
+++ section21/12-adv-starting-project/frontend/src/pages/Error.jsx
@@ -1,5 +1,27 @@
+import { useRouteError } from "react-router-dom"
+
+import PageContent from "../components/PageContent"
+
 function ErrorPage() {
-	return <h1>An error occurred</h1>
+	const error = useRouteError()
+
+	let title = "An error occured!"
+	let message = "something went wrong"
+
+	if (error.status === 500) {
+		message = JSON.parse(error).data.message
+	}
+
+	if (error.status === 404) {
+		title = "404"
+		message = "Cold not find"
+	}
+
+	return (
+		<PageContent title={title}>
+			<p>{message}</p>
+		</PageContent>
+	)
 }
 
 export default ErrorPage

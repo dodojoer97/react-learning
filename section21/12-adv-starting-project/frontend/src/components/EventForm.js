c5a325a4d70c77548649c158248e43b98cc5d235
Ido <ido.sarue@travelfactory.fr>
2024-10-06 16:14:57 +0300

    learn about useRouteLoaderData

--- section21/12-adv-starting-project/frontend/src/components/EventForm.js
+++ section21/12-adv-starting-project/frontend/src/components/EventForm.js
@@ -9,7 +9,9 @@ function EventForm({ method, event }) {
 	}
 
 	return (
-		<Form className={classes.form}>
+		<Form
+			method='post'
+			className={classes.form}>
 			<p>
 				<label htmlFor='title'>Title</label>
 				<input

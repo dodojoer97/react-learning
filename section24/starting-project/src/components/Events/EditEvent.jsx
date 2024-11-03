b2fe30b3ec26534311ecc88cb9709dd0ea0c2b84
Ido <ido.sarue@travelfactory.fr>
2024-11-03 15:22:14 +0200

    continue

--- section24/starting-project/src/components/Events/EditEvent.jsx
+++ section24/starting-project/src/components/Events/EditEvent.jsx
@@ -1,27 +1,66 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate, useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx"
+import EventForm from "./EventForm.jsx"
+import { fetchEvent } from "../../util/http.js"
+import LoadingIndicator from "../UI/LoadingIndicator.jsx"
+import ErrorBlock from "../UI/ErrorBlock.jsx"
 
 export default function EditEvent() {
-  const navigate = useNavigate();
+	const navigate = useNavigate()
+	const { id } = useParams()
 
-  function handleSubmit(formData) {}
+	const { data, isPending, isError, error } = useQuery({
+		queryKey: ["events", id],
+		queryFn: ({ signal }) => fetchEvent({ id, signal }),
+	})
 
-  function handleClose() {
-    navigate('../');
-  }
+	function handleSubmit(formData) {}
 
-  return (
-    <Modal onClose={handleClose}>
-      <EventForm inputData={null} onSubmit={handleSubmit}>
-        <Link to="../" className="button-text">
-          Cancel
-        </Link>
-        <button type="submit" className="button">
-          Update
-        </button>
-      </EventForm>
-    </Modal>
-  );
+	function handleClose() {
+		navigate("../")
+	}
+
+	let content;
+
+	if(isPending) {
+		content = <div className="center">
+			<LoadingIndicator />
+		</div>
+	}
+
+	if(isError) {
+		content = <>
+			<ErrorBlock title={'error'} message={error.message}/>
+			<div className="form-actions">
+				<Link to="../" className="button">
+					Okay
+				</Link>
+			</div>
+		</>
+	}
+
+	if(data) {
+		content = <EventForm
+		inputData={data}
+		onSubmit={handleSubmit}>
+		<Link
+			to='../'
+			className='button-text'>
+			Cancel
+		</Link>
+		<button
+			type='submit'
+			className='button'>
+			Update
+		</button>
+	</EventForm>
+	}
+
+	return (
+		<Modal onClose={handleClose}>
+			{content}
+		</Modal>
+	)
 }

570db69b128355f9d49c2abd02f603ffbd98e72c
Ido <ido.sarue@travelfactory.fr>
2024-11-03 15:40:37 +0200

    learn about optimisit updates

--- section24/starting-project/src/components/Events/NewEventsSection.jsx
+++ section24/starting-project/src/components/Events/NewEventsSection.jsx
@@ -9,10 +9,10 @@ import { fetchEvents } from "../../util/http.js";
 
 export default function NewEventsSection() {
   const {data, isPending, isError, error} = useQuery({
-    queryKey: ['events'],
-    queryFn: fetchEvents,
+    queryKey: ['events', {max: 3}],
+    queryFn: ({signal, queryKey}) => fetchEvents({signal, ...queryKey[1]}),
     staleTime: 5000,
-    gcTime: 30000 // 30 secs
+    // gcTime: 30000 // 30 secs
   })
 
   let content;

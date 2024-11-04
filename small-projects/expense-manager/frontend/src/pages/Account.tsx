3683a122cadaab54f32c24b004b1a0477ac33c6b
ido sarue <dodojoker67@gmail.com>
2024-11-04 21:05:01 +0200

    add basic styling

--- small-projects/expense-manager/frontend/src/pages/Account.tsx
+++ small-projects/expense-manager/frontend/src/pages/Account.tsx
@@ -1,76 +1,147 @@
 import React, { useState } from "react";
+import type { ChangeEvent } from "react";
+
+// Redux
+import { useDispatch, useSelector } from "react-redux";
+
+// UI Components
+import Input from "@/components/UI/Input";
+
+// Util
+import { isEmail, hasMinLength } from "@/utils/utils";
+
+// Store
+import { login, clearError } from "@/store/authSlice";
+import { RootState, AppDispatch } from "@/store/store";
+
+// Hooks
+import useInput from "@/hooks/useInput";
+import InputError from "@/components/UI/InputError";
+
+// Translations
+import { useTranslation } from "react-i18next";
+import Form from "@/components/UI/Form";
+import Button from "@/components/UI/Button";
 
 const AccountPanel: React.FC = () => {
+	// Translations
+	const { t } = useTranslation(["signup", "forms"]);
+
+	// Redux
+	const dispatch = useDispatch<AppDispatch>();
+
+	// Form fields
+	const emailField = useInput<HTMLInputElement, string>({
+		defaultValue: "",
+		validationFn: (value: string) => {
+			return isEmail(value);
+		},
+		clearErrorFN: () => dispatch(clearError()),
+	});
+
+	// Name field
+	const nameField = useInput<HTMLInputElement, string>({
+		defaultValue: "",
+		validationFn: (value: string) => hasMinLength(value, 3),
+		clearErrorFN: () => dispatch(clearError()), // Dispatch clear error action
+	});
+
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>): void {}
+
 	return (
 		<div className="bg-white dark:bg-gray-800 shadow-sm rounded-xl mb-8">
-			<div className="grow">
-				{/* Panel body */}
-				<div className="p-6 space-y-6">
-					<h2 className="text-2xl text-gray-800 dark:text-gray-100 font-bold mb-5">
-						My Account
-					</h2>
-					{/* Personal Info */}
-					<section>
-						<h2 className="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
-							Personal Info
+			<Form onSubmit={(e) => handleSubmit(e)}>
+				<div className="grow">
+					{/* Panel body */}
+					<div className="p-6 space-y-6">
+						<h2 className="text-2xl text-gray-800 dark:text-gray-100 font-bold mb-5">
+							My Account
 						</h2>
-						<div className="text-sm">
-							Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
-							officia deserunt mollit.
-						</div>
-						<div className="sm:flex sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5">
-							<div className="sm:w-1/3">
-								<label className="block text-sm font-medium mb-1" htmlFor="name">
-									Name
-								</label>
-								<input id="name" className="form-input w-full" type="text" />
+						{/* Personal Info */}
+						<section>
+							<h2 className="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
+								Personal Info
+							</h2>
+							<div className="text-sm">
+								Excepteur sint occaecat cupidatat non proident, sunt in culpa qui
+								officia deserunt mollit.
 							</div>
-						</div>
-					</section>
-					{/* Email */}
-					<section>
-						<h2 className="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
-							Email
-						</h2>
-						<div className="text-sm">
-							Excepteur sint occaecat cupidatat non proident sunt in culpa qui
-							officia.
-						</div>
-						<div className="flex flex-wrap mt-5">
-							<div className="mr-2">
-								<label className="sr-only" htmlFor="email">
-									Business email
-								</label>
-								<input id="email" className="form-input" type="email" />
+							<div className="sm:flex sm:items-center space-y-4 sm:space-y-0 sm:space-x-4 mt-5">
+								<div className="sm:w-1/3">
+									<Input
+										id="name"
+										type="text"
+										label={"Name"}
+										required
+										value={nameField.value}
+										onChange={(e) =>
+											nameField.handleInputChange(
+												e as ChangeEvent<HTMLInputElement>
+											)
+										}
+										onBlur={nameField.handleInputBlur}
+									></Input>
+									{nameField.hasError && <InputError message={"Some error"} />}
+								</div>
 							</div>
-						</div>
-					</section>
-					{/* Password */}
-					<section>
-						<h2 className="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
-							Password
-						</h2>
-						<div className="mt-5">
-							<button className="btn border-gray-200 dark:border-gray-700/60 shadow-sm text-violet-500">
-								Set New Password
-							</button>
-						</div>
-					</section>
-				</div>
-				{/* Panel footer */}
-				<footer>
-					<div className="flex flex-col px-6 py-5 border-t border-gray-200 dark:border-gray-700/60">
-						<div className="flex self-end">
-							<button className="btn dark:bg-gray-800 border-gray-200 dark:border-gray-700/60 hover:border-gray-300 dark:hover:border-gray-600 text-gray-800 dark:text-gray-300">
-								Cancel
-							</button>
-							<button className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white ml-3">
-								Save Changes
-							</button>
-						</div>
+						</section>
+						{/* Email */}
+						<section>
+							<h2 className="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
+								Email
+							</h2>
+							<div className="text-sm">
+								Excepteur sint occaecat cupidatat non proident sunt in culpa qui
+								officia.
+							</div>
+							<div className="flex flex-wrap mt-5">
+								<div className="mr-2">
+									<Input
+										id="email"
+										type="email"
+										label={t("forms:enterEmail")}
+										required
+										value={emailField.value}
+										onChange={(e) =>
+											emailField.handleInputChange(
+												e as ChangeEvent<HTMLInputElement>
+											)
+										}
+										onBlur={emailField.handleInputBlur}
+									></Input>
+									{emailField.hasError && (
+										<InputError message={t("forms:noEmailMatching")} />
+									)}
+								</div>
+							</div>
+						</section>
+						{/* Password */}
+						<section>
+							<h2 className="text-xl leading-snug text-gray-800 dark:text-gray-100 font-bold mb-1">
+								Password
+							</h2>
+							<div className="mt-5">
+								<Button className="btn border-gray-200 dark:border-gray-700/60 shadow-sm text-violet-500">
+									Set New Password
+								</Button>
+							</div>
+						</section>
 					</div>
-				</footer>
-			</div>
+					{/* Panel footer */}
+					<footer>
+						<div className="flex flex-col px-6 py-5 border-t border-gray-200 dark:border-gray-700/60">
+							<div className="flex self-end">
+								<Button className="btn dark:bg-gray-800 border-gray-200 dark:border-gray-700/60 hover:border-gray-300 dark:hover:border-gray-600 text-gray-800 dark:text-gray-300">
+									Cancel
+								</Button>
+								<Button className="btn bg-gray-900 text-gray-100 hover:bg-gray-800 dark:bg-gray-100 dark:text-gray-800 dark:hover:bg-white ml-3">
+									Save Changes
+								</Button>
+							</div>
+						</div>
+					</footer>
+				</div>
+			</Form>
 		</div>
 	);
 };

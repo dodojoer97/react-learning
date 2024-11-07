090c8287ce58b4af43dc33e501b53bc21e2d7797
ido sarue <dodojoker67@gmail.com>
2024-11-07 20:00:18 +0200

    add disabled

--- small-projects/expense-manager/common/build/models/User.d.ts
+++ small-projects/expense-manager/common/build/models/User.d.ts
@@ -2,7 +2,8 @@ export declare class User {
     uid: string;
     email: string;
     displayName: string;
+    currency: string;
     password?: string | undefined;
-    constructor(uid: string, email: string, displayName: string, password?: string | undefined);
+    constructor(uid: string, email: string, displayName: string, currency: string, password?: string | undefined);
 }
 //# sourceMappingURL=User.d.ts.map
--- small-projects/expense-manager/common/build/models/User.js
+++ small-projects/expense-manager/common/build/models/User.js
@@ -2,10 +2,11 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 class User {
-    constructor(uid, email, displayName, password) {
+    constructor(uid, email, displayName, currency, password) {
         this.uid = uid;
         this.email = email;
         this.displayName = displayName;
+        this.currency = currency;
         this.password = password;
     }
 }

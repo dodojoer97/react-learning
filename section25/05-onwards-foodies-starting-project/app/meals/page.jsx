604190aeb3755368934951939cb6175405e6da35
Ido <ido.sarue@travelfactory.fr>
2024-11-14 16:52:32 +0200

    add components

--- section25/05-onwards-foodies-starting-project/app/meals/page.jsx
+++ section25/05-onwards-foodies-starting-project/app/meals/page.jsx
@@ -1,5 +1,22 @@
+import Link from "next/link"
+import classes from "./page.module.css"
+
+
+import MealsGrid from "@/components/meals/meals-grid"
+
 export default function MealsPage() {
     return <>
-        <h1>Meals</h1>
+        <header className={classes.header}>
+            <h1>Meals <span className={classes.highlight}>teext</span></h1>
+            <p>Lorem, ipsum.</p>
+            <p className={classes.cta}>
+                <Link href="/meals/share">
+                    Lorem ipsum dolor sit.
+                </Link>
+            </p>
+        </header>
+        <main className={classes.main}>
+            <MealsGrid meals={[]}/>
+        </main>
     </>
 }

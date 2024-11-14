aaffb1179aa3f353ccc30329ef4aa7f6702e3806
Ido <ido.sarue@travelfactory.fr>
2024-11-14 17:58:30 +0200

    continue learning

--- section25/05-onwards-foodies-starting-project/app/meals/[id]/page.jsx
+++ section25/05-onwards-foodies-starting-project/app/meals/[id]/page.jsx
@@ -1,6 +1,28 @@
-export default function MealPage({params}) {
+import Image from "next/image"
+import classes from './page.module.css'
+
+import {getMeal} from "@/lib/meals"
+
+export default async function MealPage({params}) {
     const {id} = params
+    const meal = await getMeal(id)
     return <>
-        <h1>Meal {id}</h1>
+    {id}
+        <header className={classes.header}>
+            <div className={classes.image}>
+                <Image src={meal.image} alt={meal.title} width={100} height={100}/>
+            </div>
+            <div className={classes.headerText}>
+                <h1>{meal.title}</h1>
+                <p className={classes.creator}>
+                    by <a href={`mailto${meal.creator_email}`}>{meal.creator}</a>
+                </p>
+            
+                <p className={classes.summary}>{meal.summary}</p>
+            </div>
+        </header>
+        <main>
+            {/* <p className={classes.instructions} dangerouslySetInnerHTML={{__html: meal.instructions}}></p> */}
+        </main>
     </>
 }
--- section25/05-onwards-foodies-starting-project/lib/meals.js
+++ section25/05-onwards-foodies-starting-project/lib/meals.js
@@ -3,8 +3,11 @@ import sql from "better-sqlite3"
 const db = sql("meals.db")
 
 export async function getMeals() {
-    await new Promise((resolve) => setTimeout(() => {resolve()}, 2000))
-
     const meals = db.prepare("SELECT * FROM meals").all() 
     return meals
+}
+
+
+export async function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug) 
 }

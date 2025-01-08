266d207a3371600115f87fe2a5050a3293a7a4ae
Ido <ido.sarue@travelfactory.fr>
2025-01-08 19:08:19 +0200

    add error boundry

--- section27/01-starting-project/app/page.js
+++ section27/01-starting-project/app/page.js
@@ -1,17 +1,32 @@
+import fs from "node:fs/promises"
+
 import ClientDemo from "@/components/ClientDemo"
 import RSCDemo from "@/components/RSCDemo"
 
 import DataFetchingDemo from "@/components/DataFetchingDemo"
 import ServerActionsDemo from "@/components/ServerActionsDemo"
+import UsePromiseDemo from "@/components/UsePromisesDemo"
+import ErrorBoundary from "@/components/ErrorBoundary"
 
-export default function Home() {
+import { Suspense } from "react"
+
+export default async function Home() {
+	const fetchUsersPromise = new Promise((resolve, reject) =>
+		setTimeout(async () => {
+			const data = await fs.readFile("dummy-db.json", "utf-8")
+			const users = JSON.parse(data)
+			// resolve(users)
+			reject(new Error("error"))
+		}, 2000)
+	)
+
 	return (
 		<main>
-			<ClientDemo>
-				<RSCDemo />
-			</ClientDemo>
-			<DataFetchingDemo />
-			<ServerActionsDemo />
+			<ErrorBoundary fallback='Went wrong'>
+				<Suspense fallback={"...loading"}>
+					<UsePromiseDemo usersPromise={fetchUsersPromise} />
+				</Suspense>
+			</ErrorBoundary>
 		</main>
 	)
 }

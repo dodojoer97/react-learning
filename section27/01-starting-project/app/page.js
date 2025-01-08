1fe85602c7d0fcf28addc3e86da78d906dfd9681
Ido <ido.sarue@travelfactory.fr>
2025-01-08 18:56:43 +0200

    add more starting files:

--- section27/01-starting-project/app/page.js
+++ section27/01-starting-project/app/page.js
@@ -1,12 +1,17 @@
 import ClientDemo from "@/components/ClientDemo"
 import RSCDemo from "@/components/RSCDemo"
 
+import DataFetchingDemo from "@/components/DataFetchingDemo"
+import ServerActionsDemo from "@/components/ServerActionsDemo"
+
 export default function Home() {
 	return (
 		<main>
 			<ClientDemo>
 				<RSCDemo />
 			</ClientDemo>
+			<DataFetchingDemo />
+			<ServerActionsDemo />
 		</main>
 	)
 }

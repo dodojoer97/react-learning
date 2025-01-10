982bc3c9b67eda22dbb513a5bf50a20e83567add
ido sarue <dodojoker67@gmail.com>
2025-01-10 13:00:33 +0200

    add animation

--- section28/01-starting-project/src/components/ChallengeItem.jsx
+++ section28/01-starting-project/src/components/ChallengeItem.jsx
@@ -1,66 +1,61 @@
-import { useContext } from "react"
+import { useContext } from "react";
+import { motion } from "framer-motion";
 
-import { ChallengesContext } from "../store/challenges-context.jsx"
+import { ChallengesContext } from "../store/challenges-context.jsx";
 
-export default function ChallengeItem({
-	challenge,
-	onViewDetails,
-	isExpanded,
-}) {
-	const { updateChallengeStatus } = useContext(ChallengesContext)
+export default function ChallengeItem({ challenge, onViewDetails, isExpanded }) {
+	const { updateChallengeStatus } = useContext(ChallengesContext);
 
-	const formattedDate = new Date(challenge.deadline).toLocaleDateString(
-		"en-US",
-		{
-			day: "2-digit",
-			month: "short",
-			year: "numeric",
-		}
-	)
+	const formattedDate = new Date(challenge.deadline).toLocaleDateString("en-US", {
+		day: "2-digit",
+		month: "short",
+		year: "numeric",
+	});
 
 	function handleCancel() {
-		updateChallengeStatus(challenge.id, "failed")
+		updateChallengeStatus(challenge.id, "failed");
 	}
 
 	function handleComplete() {
-		updateChallengeStatus(challenge.id, "completed")
+		updateChallengeStatus(challenge.id, "completed");
 	}
 
 	return (
 		<li>
-			<article className='challenge-item'>
+			<article className="challenge-item">
 				<header>
 					<img {...challenge.image} />
-					<div className='challenge-item-meta'>
+					<div className="challenge-item-meta">
 						<h2>{challenge.title}</h2>
 						<p>Complete until {formattedDate}</p>
-						<p className='challenge-item-actions'>
-							<button
-								onClick={handleCancel}
-								className='btn-negative'>
+						<p className="challenge-item-actions">
+							<button onClick={handleCancel} className="btn-negative">
 								Mark as failed
 							</button>
 							<button onClick={handleComplete}>Mark as completed</button>
 						</p>
 					</div>
 				</header>
-				<div className={`${isExpanded && "expanded"} challenge-item-details`}>
+				<div className={`challenge-item-details`}>
 					<p>
 						<button onClick={onViewDetails}>
 							View Details{" "}
-							<span className='challenge-item-details-icon'>&#9650;</span>
+							<motion.span
+								animate={{ rotate: isExpanded ? 180 : 0 }}
+								className="challenge-item-details-icon"
+							>
+								&#9650;
+							</motion.span>
 						</button>
 					</p>
 
 					{isExpanded && (
 						<div>
-							<p className='challenge-item-description'>
-								{challenge.description}
-							</p>
+							<p className="challenge-item-description">{challenge.description}</p>
 						</div>
 					)}
 				</div>
 			</article>
 		</li>
-	)
+	);
 }

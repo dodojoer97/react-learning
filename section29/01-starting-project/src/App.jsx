359129919d48c6d3257737cff0f89c72dd354e71
ido sarue <dodojoker67@gmail.com>
2025-01-25 16:09:36 +0200

    start learning about compound components

--- section29/01-starting-project/src/App.jsx
+++ section29/01-starting-project/src/App.jsx
@@ -1,5 +1,27 @@
+import Accordion from "./components/Accordion/Accordion";
+
 function App() {
-  return <h1>React Patterns & Practices</h1>;
+	return (
+		<main>
+			<section>
+				<h2>Why work with us?</h2>
+				<Accordion className="accordion">
+					<Accordion.Item title="We got 20 years" id={1}>
+						<article>
+							<p>You can't ....</p>
+							<p>Lorem.</p>
+						</article>
+					</Accordion.Item>
+					<Accordion.Item title="blablabla" id={2}>
+						<article>
+							<p>Lorem, ipsum.</p>
+							<p>Lorem.</p>
+						</article>
+					</Accordion.Item>
+				</Accordion>
+			</section>
+		</main>
+	);
 }
 
 export default App;
--- section29/01-starting-project/src/components/Accordion/AccordionItem.jsx
+++ section29/01-starting-project/src/components/Accordion/AccordionItem.jsx
@@ -1,6 +1,21 @@
-export default function AccordionItem({className, title, children}) {
-    return <li className={className}>
-        <h3>{title}</h3>
-        <div>{children}</div>
-    </li>
+import { useAccordionContext } from "./Accordion";
+
+export default function AccordionItem({ id, className, title, children }) {
+	const { openItemId, toggleItem } = useAccordionContext();
+
+	const isOpen = openItemId === id;
+
+	function handleClick() {
+		toggleItem(id);
+	}
+
+	return (
+		<li className={className}>
+			<h3 onClick={handleClick}>{title}</h3>
+
+			<div className={`accordion-item-content ${isOpen ? "open" : undefined}`}>
+				{children}
+			</div>
+		</li>
+	);
 }

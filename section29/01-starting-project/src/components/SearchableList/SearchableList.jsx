21c07581014a10fb80faa6f100afce6b81a527cc
ido sarue <dodojoker67@gmail.com>
2025-02-01 18:36:24 +0200

    Add debouncing

--- section29/01-starting-project/src/components/SearchableList/SearchableList.jsx
+++ section29/01-starting-project/src/components/SearchableList/SearchableList.jsx
@@ -1,20 +1,32 @@
-import { useState } from "react"
+import { useState, useRef } from "react";
 
-export default function SearchableList({items, children}) {
-    const [searchTerm, setSearchTerm] = useState("") 
+export default function SearchableList({ items, children }) {
+	const [searchTerm, setSearchTerm] = useState("");
+	const lastChange = useRef("");
 
-    const searchResults = items.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()))
+	const searchResults = items.filter((item) =>
+		item.title.toLowerCase().includes(searchTerm.toLowerCase())
+	);
 
-    function handleChange(e) {
-        setSearchTerm(e.target.value)
-    }
-    
-    
-    return <div className="searchable-list">
-        <input type="search" placeholder="search" onChange={handleChange}/>
-        <ul>
-            {searchResults.map((item, index) => <li key={index}>{children(item)}</li>)}
-        </ul>
-    </div>
+	function handleChange(e) {
+		if (lastChange.current) {
+			clearTimeout(lastChange.current);
+		}
 
+		lastChange.value = setTimeout(() => {
+			lastChange.current = null;
+			setSearchTerm(e.target.value);
+		}, 500);
+	}
+
+	return (
+		<div className="searchable-list">
+			<input type="search" placeholder="search" onChange={handleChange} />
+			<ul>
+				{searchResults.map((item, index) => (
+					<li key={index}>{children(item)}</li>
+				))}
+			</ul>
+		</div>
+	);
 }

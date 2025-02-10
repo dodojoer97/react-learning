18ebc1d21e25a94ba7629f710b30589e65fa44ab
Ido <ido.sarue@travelfactory.fr>
2025-02-10 15:17:52 +0200

    Add async test

---
+++ section31/01-starting-project/src/components/Async.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import Async from './Async';
+
+describe("Async component", () => {
+    test("renders posts", async () => {
+        // Arrange
+        render(<Async />)
+        
+        // Act
+
+        // Assert
+        const listItemElements = await screen.findAllByRole("listitem")
+        expect(listItemElements).not.toHaveLength(0)
+    })
+})

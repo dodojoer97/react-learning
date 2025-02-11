aee6fbf20c540bcf52c8e7d134d05017d215fa24
Ido <ido.sarue@travelfactory.fr>
2025-02-11 15:11:36 +0200

    learn to mocK

--- section31/01-starting-project/src/components/Async.test.js
+++ section31/01-starting-project/src/components/Async.test.js
@@ -3,6 +3,18 @@ import Async from './Async';
 
 describe("Async component", () => {
     test("renders posts", async () => {
+        window.fetch = jest.fn(); 
+        window.fetch.mockResolvedValueOnce({
+            json: async () => {
+                return [
+                    {
+                        id: 'p1',
+                        title: "first mock post"
+                    }
+                ]
+            }
+        })
+
         // Arrange
         render(<Async />)
         
